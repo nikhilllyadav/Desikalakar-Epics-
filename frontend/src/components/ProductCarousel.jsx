4398c4504190fa34d9bfc233df09294a58ee6657
nikhilllyadav <77nikhilyadav77@gmail.com>
2024-05-08 09:06:18 +0530

    Complete

---
+++ frontend/src/components/ProductCarousel.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import { Carousel, Image } from 'react-bootstrap';
+import Message from './Message';
+import { useGetTopProductsQuery } from '../slices/productsApiSlice';
+
+const ProductCarousel = () => {
+  const { data: products, isLoading, error } = useGetTopProductsQuery();
+
+  return(
+    < div style={{color:'white'}}> Hello</div>
+  );
+};
+
+export default ProductCarousel;
